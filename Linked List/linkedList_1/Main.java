ccd04074bcadcf0e3b2529cc064728464e3bd2c6
JaiSonii <138607981+JaiSonii@users.noreply.github.com>
2024-01-10 01:06:27 +0530

    Update Main.java

--- Linked List/linkedList_1/Main.java
+++ Linked List/linkedList_1/Main.java
@@ -5,22 +5,50 @@ public class Main {
     public static void main(String[] args) {
 
         LinkedList myLinkedList = new LinkedList(4);
-        myLinkedList.Append(1);
-        myLinkedList.Append(5);
-
+        // for (int i = 1; i <= 5; i++) {
+        // myLinkedList.Append(i);
+        // }
+        // myLinkedList.removeFromFirst();
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
 
-        System.out.println("Before removing: ");
+        // System.out.println("Before removing: ");
         System.out.println("\nLinked List:");
         myLinkedList.printList();
 
-        myLinkedList.removeFromLast();
+        // myLinkedList.insert(4, 91);
 
-        System.out.println("After removing: ");
-        System.out.println("\nLinked List:");
-        myLinkedList.printList();
+        // myLinkedList.remove(3);
+
+        myLinkedList.reverse();
+
+        // System.out.println("Node from index 3");
+        // System.out.println(myLinkedList.get(3).value);
+
+        // System.out.println("Node from index -1");
+        // System.out.println(myLinkedList.get(-1));
+
+        // System.out.println("Node from index 7");
+        // System.out.println(myLinkedList.get(7));
+
+        // System.out.println("Set Method: ");
+        // myLinkedList.set(-1, 4);
+        // myLinkedList.set(9, 0);
+        // myLinkedList.set(4, 0);
+
+        // System.out.println("Prepend :");
+        // myLinkedList.prepend(12);
+        // System.out.println("After removing: ");
+        // System.out.println("\nLinked List:");
+        // myLinkedList.removeFromLast();
+        // myLinkedList.printList();
+        // myLinkedList.removeFromFirst();
+        // System.out.println("\nLinked List after remove from first:");
+        // myLinkedList.printList();
+        // myLinkedList.getHead();
+        // myLinkedList.getTail();
+        // myLinkedList.getLength();
 
     }
 
