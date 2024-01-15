ff52cf1122637f7567eb32f0222994389380cbc0
JaiSonii <138607981+JaiSonii@users.noreply.github.com>
2024-01-15 10:07:54 +0530

    Add files via upload

---
+++ Linked List/questions/kthNodeFromEnd/LinkedList.java
@@ -0,0 +1,95 @@
+package questions.kthNodeFromEnd;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (head == null) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("\nLinked List:");
+        if (head == null) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // WRITE FINDKTHFROMEND METHOD HERE //
+    // //
+    // //
+    // //
+    // //
+    //////////////////////////////////////
+    public Node findKthFromEnd(int k) {
+        if (head == null)
+            return null;
+        Node front = head;
+        Node back = head;
+        for (int i = 1; i < k; i++) {
+            if (front.next == null) {
+                return null;
+            }
+            front = front.next;
+        }
+        while (front.next != null) {
+            front = front.next;
+            back = back.next;
+        }
+        return back;
+    }
+
+}
